7a3214590be0c022e4df384cbfba5c69b7fb992f
Alok <alok.alps89@gmail.com>
2014-04-08 22:53:31 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chaapu::Application.config.secret_key_base = '6f4a8e977a8e1daf197d167fc3b0b38cab5810c8311162d0d3be80ed34712155b60bd4e99f949da56ffdee826bc5836da7c554b2aa18e8d004459ceabb2be0d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chaapu::Application.config.session_store :cookie_store, key: '_chaapu_session'
